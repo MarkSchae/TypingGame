f384778fbf31e4daa0d98034c59e8981e2b655df
Mark <mcschaefer26@gmail.com>
2025-11-12 09:39:18 +0200

    Bug fix (https missing) from fetch.

--- frontend/static/aliens/app.js
+++ frontend/static/aliens/app.js
@@ -30,5 +30,5 @@ async function renderPage(endpoint, templateName, targetId) {
 // Initial load
 document.addEventListener("DOMContentLoaded", () => {
     console.log('is this running?');
-    renderPage("typinggame-production.up.railway.app/aliens/", "layout.njk", "main-content");
+    renderPage("https://typinggame-production.up.railway.app/aliens/", "layout.njk", "main-content");
 });
